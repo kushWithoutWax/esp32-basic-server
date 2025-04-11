b94b7aec248b068b56aaa22b6a8fce9e91c3bc54
Dibyadwati Lahiri <dibyadwati.lahiri@gmail.com>
2025-04-11 17:03:56 +0530

    Setup OLED display with ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,19 @@
 #include <HTTPClient.h>
 #include "http_client.h"
 
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+const uint8_t clock_pin = 12;
+const uint8_t data_pin = 13;
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, clock_pin, data_pin);
 
 //Your Domain name with URL path or IP address with path
 //String serverName = "http://192.168.1.3:8081/hi";
@@ -20,9 +33,16 @@ ClientWrapper client;
 
 void setup() {
   Serial.begin(115200); 
-  client.setup();
+  //client.setup();
+  u8g2.begin();
 }
 
 void loop() {
-  client.getRequest();
+  //client.getRequest();
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB12_tr);
+    u8g2.drawStr(0,15,"Hello World");
+  } while ( u8g2.nextPage() );
+  delay(1000);
 }
